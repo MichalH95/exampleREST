a7d2b867513a0fdbb1eac2ed9d939dd4c170f2ca
Michal Húževka <michal.huzevka@gmail.com>
2021-06-24 11:06:27 +0200

    Database connection

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/MichalH95/exampleREST/database"
 	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func helloWorld(c *fiber.Ctx) {
@@ -12,9 +16,21 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/", helloWorld)
 }
 
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Connection to database opened")
+}
+
 func main() {
 	app := fiber.New()
+	initDatabase()
 
 	setupRoutes(app)
 	app.Listen(3000)
+
+	defer database.DBConn.Close()
 }
